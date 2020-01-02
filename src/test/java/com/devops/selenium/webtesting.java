3f0713bf048abb4a44c2e69ea2555f524a5535a1
Bhavani Bojarajan <balasubramaniyan.p@infosys.com>
2020-01-02 22:20:25 +0800

    Initial commit

---
+++ src/test/java/com/devops/selenium/webtesting.java
@@ -0,0 +1,32 @@
+package com.devops.selenium;
+
+import static org.testng.Assert.assertEquals;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class webtesting
+{
+    @org.testng.annotations.Test
+    public static void main(String[] args)
+    {
+    	String driverfile =System.getProperty("user.dir")+"/Driver/chromedriver";
+        System.setProperty("webdriver.chrome.driver",driverfile);
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--headless");
+        WebDriver driver = new ChromeDriver(chromeOptions);
+        driver.get("http://localhost:3001");
+        driver.findElement(By.name("login")).sendKeys("devops");
+        driver.findElement(By.name("password")).sendKeys("test");
+        driver.findElement(By.name("click")).click();
+        driver.manage().timeouts().implicitlyWait(3,TimeUnit.SECONDS);
+        String test=driver.findElement(By.name("test")).getText();
+        assertEquals(test,"Login Succeeded");
+        System.out.println("Test Succeeded!!");
+        driver.quit();
+
+    }
+}
